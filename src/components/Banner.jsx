f31bf8c7a255962646b3e06fc814ac60919a8dc5
tahsinaltabib <nabilarah33@gmail.com>
2025-09-02 08:30:42 +0600

    API add

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   let [data, setData] = useState([]);
 
   let getData = () => {
-    let info = axios.get("https://dummyjson.com/products").then((response) => {
+    axios.get("https://dummyjson.com/products").then((response) => {
       setData(response.data.products);
     })
     
@@ -17,11 +17,22 @@ const Banner = () => {
   
 
   return (
-    <div className="max-w-[1320px] mx-auto pt-5">
-      {data.map(() => (
-         <h2>Api</h2>
-       ))}
+  <div className="max-w-[1320px] mx-auto pt-15">
+    <div className="flex flex-wrap">
+      {data.map((item) => (
+        <div className="w-1/3">
+       
+           <h3 className='py-2 px-4 bg-purple-500 inline-block text-white'>{item.id}</h3>
+           <h2>{item.title}</h2>
+           <img src={item.thumbnail} alt="" />
+           <p>{item.description}</p>
+           <h5>{item.price}</h5>
+
+        </div>
+      ))}
+      
     </div>
+  </div>
   )
 }
 
